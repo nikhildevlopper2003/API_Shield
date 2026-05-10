1ed83a49a62ea97b8a696cbc836d69b343e29627
nikhildevlopper2003 <nikhilrghvsingh@gmail.com>
2026-05-10 15:44:48 +0530

    Removed all project files

--- gateway/src/controllers/health.controller.js
+++
@@ -1,7 +0,0 @@
-export const healthCheck = async (req, res) => {
-  res.status(200).json({
-    status: "OK",
-    message: "APIShield Gateway running",
-    timestamp: new Date()
-  });
-};
--- gateway/src/middlewares/error.middleware.js
+++
@@ -1,7 +0,0 @@
-export const errorMiddleware = (err, req, res, next) => {
-  console.error("Error:", err.message);
-
-  res.status(500).json({
-    message: "Internal Server Error"
-  });
-};
